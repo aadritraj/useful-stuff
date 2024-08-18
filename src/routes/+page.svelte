1504660f38d147fb80748d966769ca28518db0e8
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-08-18 16:38:41 +0200

    nuke default styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,48 +17,6 @@
 			</picture>
 		</span>
 
-		back to your new<br />SvelteKit app
+		SvelteKit without <strong>styles??</strong>
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<h2>
-		what you left uncompleted is on <strong>/list</strong>
-	</h2>
-
-	<h2>
-		get rid of the default css at <strong>src/app.css</strong> when you can
-	</h2>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
