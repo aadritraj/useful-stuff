1046d5856d02b1e800ef92bde90e1998bb0732ad
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-08-23 17:51:06 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<div>
+    <div class="flex h-screen justify-center items-center gap-4">
+        <div class="text-4xl font-medium">{$page.status}</div>
+
+        <div class="float-left h-12 w-px bg-black"></div>
+
+        <div>
+            <div class="font-semibold">{$page.error?.message}</div>
+            <a href="/" class="no-underline hover:underline">Go Home</a>
+        </div>
+    </div>
+</div>
