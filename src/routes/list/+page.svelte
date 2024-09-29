83cfbe3f8f6ea4b15aa67a7b9372bd55bd0fa4c7
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-09-29 15:30:25 +0200

    feat: reimplement basic todo-list

---
+++ src/routes/list/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	type TodoItem = {
+		text: String;
+		done: false;
+	};
+	// the localStorage key that the list will occupy
+	const STORAGE_KEY = "S_Todo";
+
+	let list = $state<TodoItem[]>([]);
+	let entry = $state<String>("");
+
+	$effect(() => {
+		let jsonString = localStorage.getItem(STORAGE_KEY);
+		let object = JSON.parse(jsonString as string);
+
+		if (!object) return;
+
+		list = object;
+	});
+
+	$effect(() => {
+		let jsonString = JSON.stringify(list);
+		localStorage.setItem(STORAGE_KEY, jsonString);
+	});
+
+	const addTodo = () => {
+		if (entry.length == 0) return;
+
+		let newTodo: TodoItem = {
+			text: entry,
+			done: false
+		};
+		list.push(newTodo);
+
+		entry = "";
+	};
+
+	const removeTodo = (index: number) => {
+		list.splice(index, 1);
+	};
+</script>
+
+<title>Local Todo-List</title>
+
+<h1>Local Todos</h1>
+<p>A Local Todo-List, stored in your Local Storage</p>
+
+<p>There are {list.length} items</p>
+
+<input type="text" bind:value={entry} placeholder="What to add?" />
+<button onclick={addTodo}>Add</button>
+
+<section>
+	{#each list as item, index}
+		<div>
+			<input type="checkbox" bind:checked={item.done} />
+			{item.text}
+			<button onclick={() => removeTodo(index)}>Remove</button>
+		</div>
+	{/each}
+</section>
