0046e568cf8259631b0222ba5def6b5e1dfd66e8
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-10-02 10:56:29 +0200

    feat: add links to pomodoro and list on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Link from "$lib/components/Link.svelte";
+</script>
+
+<h1>A SvelteKit application</h1>
+
+<Link href="/list">Todo-List</Link>
+<Link href="/pomodoro">Pomodoro</Link>
