1f08f9d22826b3751c95f8ad37018b35804a2db4
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-10-04 09:06:11 +0200

    refactor: move SVGs to seperate svelte components

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+	import Link from "$lib/svgs/Link.svelte";
+
 	export let href: string = "";
 	export let target: string = "_self";
-
-	/* might want to replace this later with a global svg store
-	heroicons outline link */
-	const linkSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
-</svg>`;
 </script>
 
 <div class="flex items-center gap-1">
 	<a {href} {target} class="underline decoration-dotted hover:decoration-solid">
 		<slot />
 	</a>
-	{@html linkSvg}
+	<Link />
 </div>
---
+++ src/lib/svgs/Link.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	/*
+		Heroicons - Outline
+		Link
+	*/
+
+	let className = "size-5";
+	export { className as class };
+
+	export let height = "1rem";
+	export let width = "1rem";
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class={className}
+	{height}
+	{width}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+	/>
+</svg>
