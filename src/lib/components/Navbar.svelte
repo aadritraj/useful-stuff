841f74181207082a92269b429314358c66987755
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-10-10 16:15:54 +0200

    feat: replace dummy nav items with source link

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,13 @@
+<script lang="ts">
+	import Link from "$lib/components/Link.svelte";
+</script>
+
 <nav class="flex w-full items-center justify-between border-b p-3">
 	<h1 class="text-xl">Useful Stuff</h1>
 
 	<div>
 		<div class="flex flex-row items-center gap-4">
-			<h2 class="text-lg">Navbar Item</h2>
-			<h2 class="text-lg">Navbar Item</h2>
+			<Link href="https://github.com/aadritraj/useful-stuff" target="_blank">GitHub</Link>
 		</div>
 	</div>
 </nav>
