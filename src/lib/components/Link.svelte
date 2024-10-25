ba0ee13f1ae0889b8f925e701164f0f3749221f8
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-10-25 17:39:51 +0200

    refactor: migrate away from deprecated slot syntax

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import Link from "$lib/svgs/Link.svelte";
 
-	let { href = "", target = "_self" } = $props();
+	let { children, href = "", target = "_self" } = $props();
 </script>
 
 <div class="flex items-center gap-1">
 	<a {href} {target} class="underline decoration-dotted hover:decoration-solid">
-		<slot />
+		{@render children?.()}
 	</a>
 	<Link />
 </div>
