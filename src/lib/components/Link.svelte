c402a112afe65daf3b1bc4a40d8abf12f5dd4e9c
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-11-14 19:43:07 +0100

    feat: spread props for Link component

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Link from "$lib/svgs/Link.svelte";
 
-	let { children, href = "", target = "_self" } = $props();
+	let { children, href = "", target = "_self", ...props } = $props();
 </script>
 
 <div class="flex items-center gap-1">
-	<a {href} {target} class="underline decoration-dotted hover:decoration-solid">
+	<a {...props} {href} {target} class="underline decoration-dotted hover:decoration-solid">
 		{@render children?.()}
 	</a>
 	<Link />
