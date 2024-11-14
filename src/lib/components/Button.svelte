9f23e8bb7484ba268e4f63963869909d5c5c5788
aadritraj <176519217+aadritraj@users.noreply.github.com>
2024-11-14 18:44:49 +0100

    feat: add new button component
    this only took a few days because i was indecisive of the styling
    
    i hate being full-stack its awesome

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { children, ...props } = $props();
+</script>
+
+<button
+	{...props}
+	class="rounded border-2 border-slate-200/50 bg-white px-2 py-2 font-sans text-slate-950 hover:bg-slate-100"
+>
+	{@render children?.()}
+</button>
